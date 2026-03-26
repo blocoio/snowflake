ae2920c899ee92bd8fd6732dfe1526d953b435f1
Sérgio Santos <me@sergiosantos.info>
2026-03-26 12:37:56 +0000

    Update logo. Prepare new release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 2
-        versionName = "0.2"
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
